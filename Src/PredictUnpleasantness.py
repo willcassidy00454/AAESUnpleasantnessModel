1f2a7f4302baa80826e21ba24af7a49edca23fe8
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-06-20 13:05:39 +0100

    Add model (currently only colouration)

---
+++ Src/PredictUnpleasantness.py
@@ -0,0 +1,26 @@
+from scipy.io import wavfile
+import Colouration
+import EDC
+import matplotlib.pyplot as plt
+
+
+def loadRIR(rir_filepath):
+    sample_rate, rir = wavfile.read(rir_filepath)
+    return sample_rate, rir
+
+
+def predictUnpleasantness(rir_filepath):
+    # Load audio channels and pre-process
+    sample_rate, spatial_rir = loadRIR(rir_filepath=rir_filepath)
+
+    # Compute metrics
+    colouration = Colouration.getColouration(spatial_rir[:, 0], sample_rate)
+
+    # Compute model
+    model_output = colouration
+
+    print(model_output)
+
+
+if __name__ == "__main__":
+    predictUnpleasantness("/Users/willcassidy/Development/GitHub/AAUnpleasantnessModel/Audio/Coloured.wav")
