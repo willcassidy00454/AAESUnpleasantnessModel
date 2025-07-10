db5e4ca4168294acab3ed7d64bbec0d6d9978d9f
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-07-10 10:54:02 +0100

    Limit flutter echo analysis to 1-20 Hz

--- Src/FlutterEcho.py
+++ Src/FlutterEcho.py
@@ -30,21 +30,26 @@ def getFlutterEchoScore(rir, sample_rate, should_show_plots=False):
     fft_size = 2 ** 9
     etc_window_duration_ms = 10.0
     energy_spectrum_freqs = np.fft.rfftfreq(fft_size, etc_window_duration_ms / 1000.0)
-    energy_spectra = np.zeros([len(energy_spectrum_freqs), num_octave_bands])
 
     octave_band_scores = np.zeros(num_octave_bands)
 
+    min_energy_freq_index = Utils.findIndexOfClosest(energy_spectrum_freqs, 1)
+    max_energy_freq_index = Utils.findIndexOfClosest(energy_spectrum_freqs, 20)
+
+    energy_spectra = np.zeros([max_energy_freq_index - min_energy_freq_index, num_octave_bands])
+
     for octave_band in range(num_octave_bands):
         energy_spectrum = Energy.getEnergySpectrum(rir_octave_bands[:, octave_band], sample_rate, fft_size, etc_window_duration_ms)
+        energy_spectrum = energy_spectrum[min_energy_freq_index:max_energy_freq_index]
         energy_spectra[:, octave_band] = energy_spectrum
         energy_mean = np.mean(energy_spectrum)
         energy_stddev = np.std(energy_spectrum)
         energy_max = np.max(energy_spectrum)
         octave_band_scores[octave_band] = (energy_max - energy_mean) / energy_stddev
 
-    flutter_echo_score = np.mean((octave_band_scores - 2.5) / 4.5) # # # this is arbitrary, remove
+    flutter_echo_score = np.mean(octave_band_scores)
 
     if should_show_plots:
-        showPlots(num_octave_bands, energy_spectra, energy_spectrum_freqs, octave_band_centres, flutter_echo_score)
+        showPlots(num_octave_bands, energy_spectra, energy_spectrum_freqs[min_energy_freq_index:max_energy_freq_index], octave_band_centres, flutter_echo_score)
 
     return flutter_echo_score
