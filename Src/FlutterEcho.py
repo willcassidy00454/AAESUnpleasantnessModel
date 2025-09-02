77ddf72c21a4a63af151562e4ee7b25e87017aa6
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-09-02 16:27:39 +0100

    Replace octave bands with single HPF and clean up

--- Src/FlutterEcho.py
+++ Src/FlutterEcho.py
@@ -2,6 +2,7 @@ import Utils
 import numpy as np
 from matplotlib import pyplot as plt
 import Energy
+from scipy.signal import butter, sosfilt
 
 
 def showEnergySpectrumPlots(num_octave_bands, energy_spectra, energy_spectrum_freqs, octave_band_centres, flutter_score):
@@ -40,17 +41,17 @@ def getFlutterEchoScore(rir, sample_rate, should_show_plots=False):
     # Truncate start of RIR up to max magnitude
     rir = rir[np.argmax(abs(rir)):]
 
-    # Get octave bands from RIR
-    rir_octave_bands, octave_band_centres = Utils.getOctaveBandsFromIR(rir, sample_rate)
-    num_octave_bands = rir_octave_bands.shape[1]
+    # High-pass RIR from 6 kHz
+    filter_order = 4
+    cutoff_Hz = 7000
+    sos = butter(2 * filter_order, cutoff_Hz, 'highpass', fs=sample_rate, output='sos')
+    rir_high_passed = sosfilt(sos, rir)
 
     # Get energy spectrum of each octave band, truncate between 2 and 20 Hz,
     # and calculate distance of max in each from mean, minus std dev
     fft_size = 2 ** 9
     etc_window_duration_ms = 10.0
     energy_spectrum_freqs = np.fft.rfftfreq(fft_size, etc_window_duration_ms / 1000.0)
-
-    octave_band_scores = np.zeros(num_octave_bands)
 
     etc_sample_rate = 1.0 / (etc_window_duration_ms / 1000.0)
 
@@ -60,24 +61,19 @@ def getFlutterEchoScore(rir, sample_rate, should_show_plots=False):
     min_energy_freq_index = int(np.floor(len(energy_spectrum_freqs) * (min_flutter_frequency / (etc_sample_rate / 2))))
     max_energy_freq_index = int(np.floor(len(energy_spectrum_freqs) * (max_flutter_frequency / (etc_sample_rate / 2))))
 
-    energy_spectra = np.zeros([max_energy_freq_index - min_energy_freq_index, num_octave_bands])
+    # Get energy time curve of the high-passed RIR
+    etc_dB, _ = Energy.getEnergyTimeCurve(rir_high_passed, sample_rate, etc_window_duration_ms)
 
-    for octave_band in range(num_octave_bands):
-        etc_dB, _ = Energy.getEnergyTimeCurve(rir_octave_bands[:, octave_band], sample_rate, etc_window_duration_ms)
+    etc_dB[:Utils.findIndexOfClosest(etc_dB, -60)] = -60 # This might not be feasible for recorded RIRs; 40 dB might be more robust
 
-        etc_dB[:Utils.findIndexOfClosest(etc_dB, -60)] = -60
+    # Get FFT of energy time curve in decibels, truncated between 2-20 Hz
+    energy_spectrum_dB = 10 * np.log10(np.abs(np.fft.rfft(etc_dB, n=fft_size)))
+    energy_spectrum_dB = energy_spectrum_dB[min_energy_freq_index:max_energy_freq_index]
 
-        energy_spectrum_dB = 10 * np.log10(np.abs(np.fft.rfft(etc_dB, n=fft_size)))
-        energy_spectrum_dB = energy_spectrum_dB[min_energy_freq_index:max_energy_freq_index]
-        energy_spectra[:, octave_band] = energy_spectrum_dB
-        energy_mean = np.mean(energy_spectrum_dB)
-        energy_stddev = np.std(energy_spectrum_dB)
-
-        octave_band_scores[octave_band] = energy_mean - energy_stddev
-
-    flutter_echo_score = np.max(octave_band_scores)
+    # Output mean of the energy in the 2-20 Hz region (might not need to perform FFT for this)
+    flutter_echo_score = np.mean(energy_spectrum_dB)
 
     if should_show_plots:
-        showEnergySpectrumPlots(num_octave_bands, energy_spectra, energy_spectrum_freqs[min_energy_freq_index:max_energy_freq_index], octave_band_centres, flutter_echo_score)
+        showEnergySpectrumPlots(energy_spectrum_dB, energy_spectrum_freqs[min_energy_freq_index:max_energy_freq_index], octave_band_centres, flutter_echo_score)
 
     return flutter_echo_score
