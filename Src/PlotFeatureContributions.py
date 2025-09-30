7941d139baf916e1c6f90f503f6efe4072438e58
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-09-30 16:35:56 +0100

    Update contributions plot

--- Src/PlotFeatureContributions.py
+++ Src/PlotFeatureContributions.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     features = ["Colour.", "Flut.", "Asym.", "Curv.", "Damp."]
-    contributions = [[0.385, 0.122, 0.161, 0.278, 0.314],
-                     [0.448, 0.379, 0.027, 0.216, 0.105]]
+    contributions = [[0.261, 0.253, 0.139, -0.472, 0.286],
+                     [0.461, -0.334, 0.026, -0.293, -0.079]]
 
     fig, axes = plt.subplots(2)
     fig.set_size_inches(4, 6)
@@ -14,8 +14,8 @@ if __name__ == "__main__":
         axes[plot_index].bar(features, prog_item_contributions, zorder=2)
         axes[plot_index].set_title("Saxophone" if plot_index else "Clapping")
         axes[plot_index].grid(zorder=0, axis="y")
-        axes[plot_index].set_ylim([0, 0.45])
-        axes[plot_index].set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5])
-        axes[plot_index].set_yticklabels(["0 %", "10 %", "20 %", "30 %", "40 %", "50 %"])
+        axes[plot_index].set_ylim([-0.45, 0.45])
+        axes[plot_index].set_yticks([-0.5, -0.25, 0.0, 0.25, 0.5])
+        axes[plot_index].set_yticklabels(["-50 %", "-25 %", "0 %", "25 %", "50 %"])
 
     plt.show()
