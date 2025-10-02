060faa9c0484b970d7047d91e0961dda3c711d6a
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-10-02 15:42:04 +0100

    Add significances to contributions plot

--- Src/PlotFeatureContributions.py
+++ Src/PlotFeatureContributions.py
@@ -6,12 +6,17 @@ if __name__ == "__main__":
     contributions = [[0.261, 0.253, 0.139, -0.472, 0.286],
                      [0.461, -0.334, 0.026, -0.293, -0.079]]
 
+    significances = [["***", "***", "***", "***", "***"],
+                     ["***", "***", "", "***", ""]]
+
     fig, axes = plt.subplots(2)
     fig.set_size_inches(4, 6)
     fig.set_layout_engine("tight")
 
     for plot_index, prog_item_contributions in enumerate(contributions):
-        axes[plot_index].bar(features, prog_item_contributions, zorder=2)
+        p = axes[plot_index].bar(features, prog_item_contributions, zorder=2)
+
+        axes[plot_index].bar_label(p, labels=significances[plot_index], label_type='edge')
         axes[plot_index].set_title("Saxophone" if plot_index else "Clapping")
         axes[plot_index].grid(zorder=0, axis="y")
         axes[plot_index].set_ylim([-0.45, 0.45])
